cfbb7800be228f7b8ba2b90016038ef3ce5f731a
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-02-21 21:45:28 +0545

    added drizzle.config.ts , made the postgres data persistance , and create user schems

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,43 @@
-import { Router } from "express";
+import { Router,Request,Response } from "express";
+import { db } from "../db";
+import { NewUser, users } from "../db/schema";
+import { eq } from "drizzle-orm";
+import bcryptjs from "bcryptjs";
 
 const authRouter = Router();
+
+interface SignupBody {
+    name: string;
+    email: string;
+    password: string;
+}
+
+authRouter.post("/signup", async (req: Request<{},{},SignupBody>, res : Response) => {
+    try {
+        //get req body
+      const {name , email , password} =   req.body;
+        //check if the user already exist 
+        const existingUser = await db.select().from(users).where(eq(users.email, email));
+        if (existingUser.length) {
+            res.status(400).json({ msg: "User with the same email already exist" });
+            return;
+        }
+        // hashed pw
+      const hashedPassword = await  bcryptjs.hash(password, 8);
+        //create a new user and store in db 
+        const newUser: NewUser = {
+            name,
+            email,
+            password: hashedPassword,
+        }
+
+        const [user] = await db.insert(users).values(newUser).returning();
+        res.status(201).json(user);
+
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+})
 
 authRouter.get("/", (req, res) => {
     res.send("Hey there! from auth");
