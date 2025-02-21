48bbbd775efd04ac57cd6bdc81ca3c3a2ff815af
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-02-21 19:36:29 +0545

    created a database mapping and auth page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import authRouter from "./routes/auth";
 
 const app = express();
 
+app.use(express.json())
+app.use("/auth", authRouter);
+
 app.get("/", (req, res) => {
-    res.send("Welcome to my app!!!!");
+    res.send("Welcome to my app");
 })
 
 
