2146cc776964c9c18e61c42ade53f864694c58c4
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-02-21 17:42:29 +0545

    added express and simple route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world!!!!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome d to my app!!!!");
+})
+
+
+app.listen(8000, () => {
+    console.log("http://localhost:8000");
+})
